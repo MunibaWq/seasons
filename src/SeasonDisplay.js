7425ef6dc4bc2614d3a5919b98f261b191060057
Muniba Waqar <muniba.wq@gmail.com>
2020-12-28 13:22:35 -0700

    styling - text and icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -12,8 +12,17 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   //   console.log(props.lat);
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(season);
-  return <div>Season Display</div>;
+  const text =
+    season === `winter` ? "Burr, it is chilly" : `Let's hit the beach`;
+  const icon = season === "winter" ? "snowflake" : "sun";
+  //   console.log(season);
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
